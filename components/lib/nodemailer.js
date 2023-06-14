49678c433797b1da4cb754a8a4f423169db8783a
aid003 <teremochek859@gmail.com>
2023-06-14 11:43:59 +0300

    secure

--- components/lib/nodemailer.js
+++ components/lib/nodemailer.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: true,
   auth: {
     user: process.env.GMAIL_EMAIL_ADDRESS,
     pass: process.env.GMAIL_APP_PASSWORD,
